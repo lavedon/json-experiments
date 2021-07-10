c5bd8ba8aec7e3d59ac03a5ed83b3d7df60a0557
Luke <lavedon@gmail.com>
2021-07-09 20:33:38 -0400

    almost done

--- Program.cs
+++ Program.cs
@@ -10,61 +10,105 @@ namespace json_experiments
     {
         static void Main(string[] args)
         {
-            var path = Environment.CurrentDirectory + ".\\lemma-response.json"; 
+            List<string> lemmas =  new List<string>() { "", "He", "", "Hammock" };
+            List<string> tokens = new List<string>() { "He", "Her", "Him", "Swing"};
+            zipLemmas(tokens, lemmas);
+        }
+
+        public static void GetLemmas()
+        {
+            var path = Environment.CurrentDirectory + ".\\lemma-response.json";
             string data = File.ReadAllText(path);
+
+            Dictionary<string, string> wordWithLemmaDict = new();
 
             using (JsonDocument json = JsonDocument.Parse(data))
             {
                 JsonElement root = json.RootElement;
                 // works;
-                /*
                 var tokens = root.EnumerateObject()
                     .Where(block => block.Value.ValueKind == JsonValueKind.Array && block.Name == "data")
                     .SelectMany(block => block.Value.EnumerateArray().Select(subBlock => subBlock.GetProperty("token").GetString()));
-                
-                */
+
                 IEnumerable<JsonElement> lemmaBlocks = root.EnumerateObject()
                     .Where(block => block.Value.ValueKind == JsonValueKind.Array && block.Name == "data")
                     .SelectMany(block => block.Value.EnumerateArray().Select(subBlock => subBlock.GetProperty("lemmatizations")));
 
                 List<JsonElement> lemmaList = new();
+                List<String> tokenList = new();
 
-            lemmaList.AddRange(lemmaBlocks);
-            List<string> lemmaWords = new();
-            for (var i = 0; i < lemmaList.Count; i++)
-            {
-                try {
-                    Console.WriteLine(lemmaList[i].ValueKind);
-                    var lemmas = lemmaList[i].EnumerateArray();
+                lemmaList.AddRange(lemmaBlocks);
 
-// @TODO make sure if lemmas is blank that a "" word gets added
-                    while (lemmas.MoveNext())
+                List<string> lemmaWords = new();
+                for (var i = 0; i < lemmaList.Count; i++)
+                {
+                    foreach (var l in lemmaWords)
                     {
-
-                        
-                   var lwordsObj = lemmas.Current.EnumerateObject();
-                   while (lwordsObj.MoveNext())
-                   {
-                       if (lwordsObj.Current.Name == "word")
-                       {
-                           Console.WriteLine("Inside Word");
-                           lemmaWords.Add(lwordsObj.Current.Value.GetProperty("lemma").GetString());
-                       } else {
-                        Console.WriteLine(lwordsObj.Current.Name);
-                       }
-
-
-                   }
-                       //  var lWord = lemmas.Current.TryGetProperty("lemma", out JsonElement lemmaElement);
+                        Console.WriteLine(l);
                     }
-                } catch {
+                    try
+                    {
+                        var lemmas = lemmaList[i].EnumerateArray();
+
+                        // @TODO make sure if lemmas is blank that a "" word gets added
+                        if (lemmaList[i].GetArrayLength() == 0)
+                        {
+
+                            lemmaWords.Add(string.Empty);
+                        }
+                        else
+                        {
+                            while (lemmas.MoveNext())
+                            {
+                                var lwordsObj = lemmas.Current.EnumerateObject();
+
+                                while (lwordsObj.MoveNext())
+                                {
+                                    if (lwordsObj.Current.Name == "word")
+                                    {
+                                        lemmaWords.Add(lwordsObj.Current.Value.GetProperty("lemma").GetString());
+                                        goto lemmaFound;
+                                    }
+                                    else
+                                    {
+                                    }
+                                }
+
+                            }
+                        }
+                    //  var lWord = lemmas.Current.TryGetProperty("lemma", out JsonElement lemmaElement);
+                    lemmaFound:;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error while enumerating lemmas");
+                        Console.WriteLine(ex);
+                    }
+                }
+
+
+
+
+
+            }
+        }
+
+        private Dictionary<string, string> zipLemmas(List<string> tokens, List<string> lemmas)
+    {
+        Dictionary<string, string> zippedLemmas = new();
+        for (var i = 0; i < tokens.Count; i++)
+        {
+            for (var y = 0; y < lemmas.Count; y++)
+            {
+                if (string.IsNullOrWhiteSpace(lemmas[y]))
+                {
+                    lemmas[y] = tokens[i];
                 }
             }
 
-
-
-
         }
+        return null;
+
     }
 }
 }
